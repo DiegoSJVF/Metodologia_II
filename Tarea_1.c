fe251a9befed3cb978a4f3e8065adfb5285c94c6
DiegoSJVF <diegosjv2002@gmail.com>
2023-09-23 18:33:17 -0300

    Cambios Tarea_1.c factoriales funcional

--- Tarea_1.c
+++ Tarea_1.c
@@ -4,12 +4,19 @@
 
 #define max_value 9223372036854775807
 
+struct fp
+{
+    unsigned long long int primo;
+    unsigned long long int apariciones;
+    struct fp *sig;
+};
+
 
 int main() {
     unsigned long long int factorial = 1;
     unsigned long long int aux;
     unsigned long long int *cifrado;
-    int i, j=0;
+    int i, j=0, k;
     FILE *file;
 
     cifrado = (unsigned long long int*) malloc(sizeof(unsigned long long int));
@@ -24,39 +31,30 @@ int main() {
         return 1;
     }
 
-    for (i = 1; ; i++) {
-        if ((factorial * i) > 4294967295) { // Si el factorial va a superar 2^32
+    for (i = 1; factorial*i < max_value ; i++) {
 
-            while(aux) {
-                j=0;
-                cifrado = (unsigned long long int*) realloc(cifrado, i * sizeof(unsigned long long int));
-                if (cifrado == NULL) {
-                    printf("No se pudo asignar memoria.\n");
-                    return 1;
-                }
-                cifrado[j] = aux % 4294967296; // Cifra el factorial en base 2^32
-                aux /= 4294967296;
-                j++;
-            }
-            for (j = 0; j < i; j++){
-                if(j == 0)
-                    fprintf(file, "%d! = %llu", i, cifrado[j]);
-                else if(cifrado[j])
-                    fprintf(file, " + %llu", cifrado[j]);
-            }
-            fprintf(file, "\n");
-            factorial *= i; // Actualiza el valor del factorial
-            aux = factorial;
-        }
-        else {
-            factorial *= i;
-            fprintf(file, "%d! = %llu\n", i, factorial);
-        }
+        factorial *= i; // Actualiza el valor del factorial
 
-        if (factorial > max_value) { // Si el factorial va a superar 2^63
-            printf("El número más grande cuyo factorial es menor o igual a 2^63 - 1 es: %d\n", i - 1);
-            break;
+        //f_p
+        aux = factorial;
+        j=0;
+        while(aux) {
+            cifrado = (unsigned long long int*) realloc(cifrado, j+1 * sizeof(unsigned long long int));
+            if (cifrado == NULL) {
+                printf("No se pudo asignar memoria.\n");
+                return 1;
+            }
+            cifrado[j] = aux % 4294967296; // Cifra el factorial en base 2^32
+            aux /= 4294967296;
+            j++;
         }
+        for (k = 0; k < j; k++){
+            if(k == 0)
+                fprintf(file, "%d! = %llu", i, cifrado[k]);
+            else if(cifrado[k])
+                fprintf(file, " + %llu", cifrado[k]);
+        }
+        fprintf(file, "\n");
     }
 
     free(cifrado);
